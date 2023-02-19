db62552940d8a64aa2b4fdebe908634910ebcc1c
Nathan Bell <nat.bell@gmail.com>
2023-02-19 13:44:54 +1100

    Base program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SplashKitSDK;
+
+public class Program
+{
+    //variables 
+    private static bool Quit = false;
+
+
+    public static void Main()
+    {
+        Window window = new Window("Space Adventures", 1400, 850);
+
+        //The loop for the running of the game
+        while (!window.CloseRequested && !Quit)
+        {
+            if (SplashKit.KeyTyped(KeyCode.EscapeKey))
+            {
+                Quit = true;
+            }
+            SplashKit.ProcessEvents();
+            window.Refresh(60);
+        }
+    }
+}
---
+++ lib/SplashKit.cs
@@ -0,0 +1,1 @@
+/Users/nathanbell/.splashkit/dotnet/SplashKit.cs
